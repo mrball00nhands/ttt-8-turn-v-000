7fc3e8a67576a380269a4926f2b50ada841412f9
Daniel Alvarez <dnielalvrz@gmail.com>
2021-05-26 16:29:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,3 +9,15 @@ end
 def input_to_index(input)
   input.to_i - 1
 end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && position_taken?(board, index) == false
+    true
+  else
+    false
+  end
+end
+
+def position_taken?(board, index)
+  !(board[index] == " " || board[index] -- "" || board[index] == nil)
+end
